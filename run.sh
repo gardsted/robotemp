76c624ef3f02d962d490cc31ffd06ebefa2bfa0c
gardsted <gardsted@gmail.com>
2019-09-09 21:02:07 +0200

    run with all params

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #lic:mpl2,2019,gardsted,github
 export robotemp=$(cd $(dirname $0); pwd)
-$robotemp/shell/robotemp.sh --robo-program=roboserve || (echo; echo did You run ./shell/install.sh?)
+$robotemp/shell/robotemp.sh -p 8833 -h localhost --robo-program=roboserve || (echo; echo did You run ./shell/install.sh?)
